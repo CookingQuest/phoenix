dcb707fbc65015f6a935165a1073896eb0941fdb
Jan Moeller <jan.moeller0@gmail.com>
2017-01-07 21:18:03 +0100

    Remove code reloading features

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,6 @@ use Mix.Config
 config :cooking_quest, CookingQuest.Endpoint,
   http: [port: 4000],
   debug_errors: true,
-  code_reloader: true,
   check_origin: false,
   watchers: []
 
--- lib/cooking_quest/endpoint.ex
+++ lib/cooking_quest/endpoint.ex
@@ -3,12 +3,6 @@ defmodule CookingQuest.Endpoint do
 
   socket "/socket", CookingQuest.UserSocket  
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
-  if code_reloading? do
-    plug Phoenix.CodeReloader
-  end
-
   plug Plug.RequestId
   plug Plug.Logger
 
