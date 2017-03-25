6e5c945a10e28c7ff0550390fbc879d4dd19fefe
Jan Moeller <jan.moeller0@gmail.com>
2017-03-25 20:32:53 +0100

    UserInfo wip

--- web/graphql/types.ex
+++ web/graphql/types.ex
@@ -7,19 +7,27 @@ defmodule CookingQuest.Schema.Types do
     field :id, :id
     field :name, :string
     field :stats, :stats do
-      resolve fn user, _, _ ->
-        batch({Schema.Helpers, :by_id, Stats}, user.id, fn batch_results ->
-          {:ok, Map.get(batch_results, user.id)}
-        end) 
-      end
+      resolve fn user, _, _ -> resolve_stats(user.id) end
     end
   end
 
   object :stats do
     field :id, :id
-    field :level, :integer
+    field :level, :float do
+      resolve fn stats, _, _ -> calc_level(stats.exp) end
+    end
     field :exp, :integer
     field :user_id, :id
     field :user, :user, resolve: assoc(:user)
+  end
+
+  defp resolve_stats(user_id) do
+    batch({Schema.Helpers, :by_id, Stats}, user_id, fn batch_results ->
+      {:ok, Map.get(batch_results, user_id)}
+    end)
+  end
+
+  defp calc_level(exp) do
+    {:ok, 0.01 * exp + 1}
   end
 end
